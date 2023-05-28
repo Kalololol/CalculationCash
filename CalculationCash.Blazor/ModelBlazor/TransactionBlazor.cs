7a63310366e578972bf7ce5693c2fa40cab6e23c
Karol kalololol <karol.binkowski92@gmail.com>
2023-05-28 18:19:34 +0200

    commit

--- CalculationCash.Blazor/ModelBlazor/TransactionBlazor.cs
+++ CalculationCash.Blazor/ModelBlazor/TransactionBlazor.cs
@@ -9,14 +9,14 @@
         public string Instrument { get; set; }
         public string TypeTransakcion { get; set; }
         public string Direction { get; set; }
-        public float Volume { get; set; }
-        public float PurchasePrice { get; set; }
+        public decimal Volume { get; set; }
+        public decimal PurchasePrice { get; set; }
         public string OrderNumber { get; set; }
-        public float Provision { get; set; }
-        public float Commision { get; set; }
-        public float Swap { get; set; }
-        public float Profit { get; set; }
-        public float ResultSum { get; set; }
+        public decimal Provision { get; set; }
+        public decimal Commision { get; set; }
+        public decimal Swap { get; set; }
+        public decimal Profit { get; set; }
+        public decimal ResultSum { get; set; }
         public Guid ReportBeforeConversionDtoId { get; set; }
     }
 }
